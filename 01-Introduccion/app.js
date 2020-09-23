895db59adce5f00a828a028f77e6719a89c17139
Juan <kaenel10@gmail.com>
2020-09-23 09:25:52 -0300

    Primer commit

---
+++ 01-Introduccion/app.js
@@ -0,0 +1,4 @@
+const nombre = prompt('Cuál es tu nombre?')
+
+document.querySelector('.contenido').innerHTML = `${nombre} está aprendiendo javascript como campeón`; //accedo a la clase contenido y reemplazo su contenido
+
