f6969dde7a596c00d86fe8ee1f53026f20a89bef
Juan <kaenel10@gmail.com>
2020-09-29 08:57:39 -0300

    Trabajando con array methods primera parte

--- 12-ArrayMethods/js/01-app.js
+++ 12-ArrayMethods/js/01-app.js
@@ -1,5 +1,7 @@
+//Arreglo tradicional, de índices
 const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
 
+//Arreglo de objetos
 const carrito = [
     { nombre: 'Monitor 27 Pulgadas', precio: 500 },
     { nombre: 'Televisión', precio: 100 },
@@ -8,3 +10,24 @@ const carrito = [
     { nombre: 'Teclado', precio: 400 },
     { nombre: 'Celular', precio: 700 },
 ]
+
+//Comprobar si un valor existe dentro de un arreglo
+
+//Forma manual
+meses.forEach( mes =>{
+    if(mes==='Enero'){
+        console.log('Enero si existe');
+    }
+});
+//Usando array method
+const resultado =meses.includes('Enero');
+console.log(resultado);
+
+//------------- En un arreglo de objetos se utiliza .some -------------
+
+const resultado2 = carrito.some( producto => producto.nombre==='Celular'); //el return queda implícito cuando es de una sola linea
+    //return producto.nombre==='Celular';
+console.log(resultado2);
+
+//.some con un array tradicional, de índices 
+const resultado3 = meses.some(mes => mes==='Mayo');
