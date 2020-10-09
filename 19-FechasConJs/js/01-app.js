52ca26203bfa72e5f40963ef36bf9f5d499e568e
Juan Kaenel <kaenel10@gmail.com>
2020-10-09 18:28:58 -0300

    Trabajando con fechas

---
+++ 19-FechasConJs/js/01-app.js
@@ -0,0 +1,14 @@
+//const diaHoy = new Date();
+// const diaHoy = new Date('December 13 2000'); //te da el dia que cae esa fecha
+const diaHoy = new Date(); 
+
+let valor;
+
+// valor = diaHoy;
+
+// valor = diaHoy.getFullYear();
+// valor = diaHoy.getHours();
+// valor = diaHoy.getMinutes();
+// valor = diaHoy.getSeconds();
+valor = diaHoy.setFullYear(1920);
+console.log(diaHoy);
