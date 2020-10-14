87a358396c0ec7f46b0cb06f362d9da1c43c2dac
Juan Kaenel <kaenel10@gmail.com>
2020-10-14 11:18:21 -0300

    Terminando con modulos

--- 28-Modulos/js/app.js
+++ 28-Modulos/js/app.js
@@ -1,7 +1,20 @@
-import {nombreCliente, ahorro, mostrarInfo, tieneSaldo} from './cliente.js';
+//import alias , {nombreCliente, ahorro, mostrarInfo, tieneSaldo,Cliente} from './cliente.js';
+import nuevaFuncion , {nombreCliente, ahorro, mostrarInfo, tieneSaldo,Cliente} from './cliente.js';
+import {Empresa} from "./empresa.js";
 //console.log(nombreCliente);
 //console.log(ahorro);
 
+//alias(); //este es el export default, nuevaFuncion() -> en este caso al ser unico puede tomar cualquier alias desde donde se lo importa y hará referencia a esa funcion
+nuevaFuncion();
 console.log(mostrarInfo(nombreCliente,ahorro));
 
 tieneSaldo(ahorro);
+
+const cliente = new Cliente(nombreCliente,ahorro);
+console.log(cliente);
+console.log(cliente.mostrarInformacion()); //accediendo desde el metodo
+
+
+const empresa = new Empresa(nombreCliente,ahorro,'ventas');
+console.log(empresa);
+console.log(empresa.mostrarInformacion());
--- 28-Modulos/js/cliente.js
+++ 28-Modulos/js/cliente.js
@@ -1,15 +1,29 @@
 export const nombreCliente = 'Juan';
 export const ahorro = 300;
 
-export function mostrarInfo(nombre,ahorro){
+export function mostrarInfo(nombre, ahorro) {
     return `Nombre: ${nombre} - Ahorro:${ahorro}`;
 }
 
-export function tieneSaldo(ahorro){
-    if(ahorro>0){
+export function tieneSaldo(ahorro) {
+    if (ahorro > 0) {
         console.log('Tiene saldo');
-    }
-    else{
+    } else {
         console.log('No tiene saldo');
     }
+}
+
+export class Cliente {
+    constructor(nombre, ahorro) {
+        this.nombre = nombre;
+        this.ahorro = ahorro;
+
+    }
+    mostrarInformacion(){
+        return `Nombre: ${this.nombre} - Ahorro:${this.ahorro}`;
+    }
+}
+/*Solo puede haber un export default por archivo. Al haber un solo export default, puede tomar el alias desde el app.js que se quiera*/
+export default function nuevaFuncion(){
+    console.log('Este es el export default');
 }
